3918cf413768ce23a5996e762e523328f6081ff4
simond-hue <achipowa205@gmail.com>
2021-08-04 11:45:37 +0200

    content update and bugfixing

--- eventhandlers.js
+++ eventhandlers.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
     var dropdown = $(".dropdown-btn");
 
-    var activeDropdown = null;
-    var activeDropdownContainer = null;
+    var activeDropdowns = [];
+    var activeDropdownContainers = [];
 
     for (var i = 0; i < dropdown.length; i++) {
         $(dropdown[i]).click(function() {
             $(this).toggleClass("active");
             if($(this).hasClass("active")){
-                activeDropdown = $(this);
+                activeDropdowns.push($(this));
                 $($(this).find('.fa')[0]).removeClass('fa-caret-right').addClass('fa-caret-down').addClass('animate');
             } 
             else{
-                activeDropdown = null;
+                activeDropdowns.pop();
                 $($(this).find('.fa')[0]).removeClass('fa-caret-down').addClass('fa-caret-right').removeClass('animate');
             }
 
             $(this).next('.dropdown-container').toggleClass("display");
-            if($(this).next('.dropdown-container').hasClass("display")) activeDropdownContainer = $(this).next('.dropdown-container');
-            else activeDropdownContainer = null;
+            if($(this).next('.dropdown-container').hasClass("display")) activeDropdownContainers.push($(this).next('.dropdown-container'));
+            else activeDropdownContainer.pop();
         })
     }
 
@@ -26,10 +26,19 @@ $(document).ready(function(){
 
     for(var i = 0; i < sidebarButtons.length; i++){
         $(sidebarButtons[i]).click(function(){
-            if(activeDropdown && activeDropdownContainer){
-                activeDropdown.removeClass("active");
-                activeDropdownContainer.removeClass("display");
+            if(activeDropdowns.length > 0 && activeDropdownContainers.length > 0){
+                removeActive(activeDropdowns,activeDropdownContainers);
             }
         })
     }
 })
+
+function removeActive(dropdowns, containers){
+    dropdowns.forEach(dropdown => {
+        dropdown.removeClass("active");
+        $(dropdown.find('.fa')[0]).removeClass('fa-caret-down').addClass('fa-caret-right').removeClass('animate');
+    });
+    containers.forEach(container => {
+        container.removeClass("display");
+    });
+}
